b4102380f7a67156ae336ab79186ae5a15900f55
Taine Jarvis <120402109+taine-j@users.noreply.github.com>
2023-10-18 14:26:04 +0100

    updated title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Wichard Bormozi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
