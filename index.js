6b1867ac644ff22cb71808782ebc0780973935fb
Taine Jarvis <120402109+taine-j@users.noreply.github.com>
2023-10-18 13:58:51 +0100

    implemented chatbot

---
+++ index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Script from 'next/script';
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Wichard Bormozi" />
+        <p className="description">
+          Visit <a href = 'https://www.botnest.co'>BotNest</a> For A Free Chatbot Prototype
+        </p>
+
+        <Script id="show-banner"
+  dangerouslySetInnerHTML={{
+    __html: ` (function(d, t) {
+        var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        v.onload = function() {
+          window.voiceflow.chat.load({
+            verify: { projectID: '652f91b3753ddb0007ec7c85' },
+            url: 'https://general-runtime.voiceflow.com',
+            versionID: 'production'
+          }).then(() => {
+            setTimeout(function() {
+              window.voiceflow.chat.open();
+            }, 1000);
+          });
+        }
+        v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+    })(document, 'script'); `,
+  }}
+/>
+      </main>
+
+      <Footer />
+
+    </div>
+  )
+}
